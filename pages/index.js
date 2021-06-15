852b091c042e6075d7eac1286115be86d3c54125
Mohamed Zidani <mzidani@sqli.com>
2021-06-15 14:06:36 +0100

    port

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
 const Index = () => (
     <div>
         <p>Sample app using React and Next.js</p>
